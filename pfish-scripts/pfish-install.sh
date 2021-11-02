c40fce4869f70fee6128a0e2ece9d78f01117b88
Amy Cash <amymcash@gmail.com>
2021-11-02 13:54:40 -0700

    Fix url for script

--- pfish-scripts/pfish-install.sh
+++ pfish-scripts/pfish-install.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 mkdir -p $HOME/bin
-wget --directory-prefix=$HOME/bin https://raw.githubusercontent.com/aquariumbio/protocols-to-edit/pfish-scripts/master/pfish-wrapper
+wget --directory-prefix=$HOME/bin https://raw.githubusercontent.com/aquariumbio/protocols-to-edit/pfish-scripts/main/pfish-wrapper
 install -m 0755 $HOME/bin/pfish-wrapper $HOME/bin/pfish
 rm -f ~/bin/pfish-wrapper
-
 
