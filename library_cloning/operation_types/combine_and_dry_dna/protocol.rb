d06b75a9b29ff06a813b5cd5b9228c07ae1c2f99
cfreer <cfreer@cs.washington.edu>
2021-11-30 16:37:29 -0800

    fixed last steps

--- library_cloning/operation_types/combine_and_dry_dna/protocol.rb
+++ library_cloning/operation_types/combine_and_dry_dna/protocol.rb
@@ -51,37 +51,34 @@ class Protocol
             }
         end
 
-        # get input volumes
-        show do
-            # check the volumes of samples for all operations, and ensure they are sufficient
-            operations.each { |op| op.temporary[:vol_P] = 1000*op.input(QUANTITY_P).val }
-            check_volumes [INPUT_P], :vol_P, :not_enough_input, check_contam: true
-            operations.each { |op| op.temporary[:vol_F] = 1000*op.input(QUANTITY_F).val }
-            check_volumes [INPUT_F], :vol_F, :not_enough_input, check_contam: true
-        end
+        # check the volumes of samples for all operations, and ensure they are sufficient
+        operations.each { |op| op.temporary[:vol_P] = 1000*op.input(QUANTITY_P).val }
+        check_volumes [INPUT_P], :vol_P, :not_enough_input, check_contam: true
+        operations.each { |op| op.temporary[:vol_F] = 1000*op.input(QUANTITY_F).val }
+        check_volumes [INPUT_F], :vol_F, :not_enough_input, check_contam: true
 
         # error operations with too little material
-        show do
-            ops=operations.running
-            operations=ops
-            if(operations.length<1)
+        ops=operations.running
+        operations=ops
+        if(operations.length<1)
+            show do
                 note "No operations running! Returning."
-                return
             end
+            return
         end
 
-        show do
-            title "Label tubes"
-            ids = operations.map { |op| op.output(OUTPUT).item.id}
-            check "Label empty tubes for the combined samples: <b>#{ids.to_sentence}</b>"
-        end
-
-        # combine correct volumes
+        # label tubes and combine correct volumes
         show do
             title "Label tubes and add insert DNA"
+            ids = operations.map { |op| op.output(OUTPUT).item.id}
+            # if ids is not an array, make it one
+            if(!ids.kind_of?(Array))
+                ids = [ids]
+            end
             warning "Be careful not to make mistakes! Work one row at a time"
+            check "Label empty tubes for the combined samples: <b>#{ids.to_sentence}</b>"
             check "Vortex Plasmid and Insert briefly and spin down before combining"
-            check "Combine the indicated volumes of the inputs into the indicated output tube(s):"
+            check "Combine the indicated volumes of the inputs into the newly labeled tubes"
         end
 
         # add plasmid backbone
@@ -104,33 +101,9 @@ class Protocol
             op.output(OUTPUT).item.associate :init_volume, vol.round(2)
         }
 
-        # dry samples. repeat for up to TRIES concentration steps (to avoid endless loop)
-        nloops=0
-        unfinished=operations.select { |op| op.output(OUTPUT).item.get(:init_volume) > MAX_VOL }
-        finished=operations.select { |op| op.output(OUTPUT).item.get(:init_volume) <= MAX_VOL }
-
-        # associate the (null) concentrating conditions for the samples that do not need concentrating
-        finished.each { |op|
-            op.output(OUTPUT).item.associate :concentration_heat_level, "none"
-            op.output(OUTPUT).item.associate :concentration_time, "none"
-        }
-
-        # return items to freezer before going to speedvac
-        in_items=[operations.map{|op| op.input(INPUT_P).item}, operations.map{|op| op.input(INPUT_F).item}].flatten
+        # return items to freezer
+        in_items=[operations.map{|op| op.input(INPUT_P).item}].flatten
         release(in_items, interactive: true, method: "boxes")
-
-        # still not concentrated enough
-        if( (nloops>=TRIES) && (unfinished.length > 1) )
-            show do
-                title "Problem!"
-                note "#{TRIES} rounds of concentration not enough for samples <b>#{unfinished.map { |op| op.output(OUTPUT).item.id}.to_sentence}</b>! Please check with a lab manager."
-            end
-        end
-
-        operations.each { |op|
-            op.output(OUTPUT).item.associate :final_volume, op.temporary[:final_volume]
-            op.output(OUTPUT).item.move_to("M20")
-        }
 
         out_items=operations.map{|op| op.output(OUTPUT).item}
         release(out_items, interactive: true, method: "boxes")
